9a5480e056fcc9569979fd818c4f8b604e49e751
Fernando Ferreira <fernando.rafael.ferreira@gmail.com>
2018-02-23 14:21:42 -0300

    Simplifies the XML Crawler to always create or update the record with latest information.

--- xeretas/crawl-xml.js
+++ xeretas/crawl-xml.js
@@ -1,6 +1,5 @@
 const request = require('request-promise-native');
 const cheerio = require('cheerio');
-const date = require('date-and-time');
 const S = require('string');
 
 const scrape = require('../util/scrape');
@@ -17,66 +16,58 @@ function createRequest(options) {
 
     if (!request['transform']) {
         // Load the XML body with cheerio
-        request['transform'] = function(body) {
+        request['transform'] = function(body, response, resolveWithFullResponse) {
             // Save the raw response to the disk
-            var file = S(request.url)
-                .chompLeft('https://').chompLeft('http://')
-                .replaceAll('/', '_').replaceAll(':', '');
-            writeText(body, 'data/' + file + ".xml");
+            var filename = S(request.url).chompLeft('https://')
+                .chompLeft('http://').replaceAll('/', '_').replaceAll(':', '');
+            writeText(body, 'data/' + filename + ".xml");
 
-            return scrape.xml(options.select).as(options.schema)
-                         .scrape(body);
+            // Scrape the body and return
+            var scraped = scrape.xml(options.select)
+                            .as(options.schema).scrape(body);
+            if (resolveWithFullResponse) {
+                response.scraped = scraped;
+                return response;
+            } else {
+                return scraped;
+            }
         };
 
     } else {
-        throw Error('Conflicting request.transform already defined.');
+        console.warn("Transformation overriden in request.transform. " +
+                     "Make sure to scrape response yourself.");
     }
 
     return request;
 }
 
-function filterNew(response, object) {
-    function datesEqual(d1, d2) {
-        d1 = d1 ? date.parse(d1, 'YYYY-MM-DD').valueOf() : false;
-        d2 = d2 ? d2.valueOf() : false;
-        return d1 == d2;
-    }
-
-    var filtered = {};
-    Object.keys(response).forEach((field) => {
-        filtered[field] = response[field];
-
-        if (object && object[field] != undefined) {
-            if (object[field] == filtered[field] ||
-                    datesEqual(object[field], filtered[field])) {
-                // Ignore fields that haven't changed.
-                delete filtered[field];
-
-            } else if (filtered[field] != undefined) {
-                console.warn(object.constructor.name, object['id'] + "'s",
-                    "[" + field +"]", typeof object[field],
-                    "\"" + object[field] + "\"", "differs from the",
-                    typeof filtered[field], "\"" + filtered[field] + "\" received.");
-
-                if (typeof object[field] == Date) {
-                    console.log("Date:", object[field].getTime());
-                }
-
-                // Ignore fields that are different.
-                // TODO Keep both versions...
-                delete filtered[field];
+function findAndUpdateOrCreate(findOrCreate, record) {
+    var promise = findOrCreate(record)
+        .spread((object, created) => {
+            if (created) {
+                console.debug("Created", object.id);
+                return [object, record];
+            } else {
+                return object.update(record).then(
+                    (object) => {
+                        console.debug("Updated", object.id,
+                            "with the latest data.");
+                        return [object, record];
+                    }
+                );
             }
-        }
+        });
 
-        if (filtered[field] == undefined || isNaN(filtered[field])) {
-            delete filtered[field];
-        }
-    });
-
-    return filtered;
+    return promise;
 }
 
 function crawlXml(options) {
+    const defaultOptions = {
+        spread: []
+    };
+
+    options = Object.assign({}, defaultOptions, options);
+
     return function() {
         return request(createRequest(options))
             .catch((error) => {
@@ -86,28 +77,14 @@ function crawlXml(options) {
                 var promises = [];
 
                 records.forEach((record, i) => {
-                    var promise = options.findOrCreate(record).spread(
-                        function(object, created) {
-                            if (!created) {
-                                var changed = filterNew(record, object);
-                                // Update the record with all NEW fields
-                                if (Object.keys(changed).length !== 0) {
-                                    var update = object.update(changed)
-                                        .then((obj) => console.debug("Updated:", changed))
-                                        .error((e) => console.error("Error:", e));
-                                    return [record, update, changed];
+                    var promise = findAndUpdateOrCreate(options.findOrCreate, record);
 
-                                } else {
-                                    console.info("" + object.id, "didn't change.");
-                                }
+                    // Chain all options.spread handlers in sequence.
+                    for (handler in options.spread) {
+                        promise = promise.spread(handler);
+                    }
 
-                            } else {
-                                console.debug("Created:", object.dataValues);
-                            }
-
-                            return [record, object];
-                        }
-                    ).catch((error) => {
+                    promise.catch((error) => {
                         console.error(error);
                     });
 
