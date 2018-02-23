7b3ef59755d3ff0d00a146486b9c598647ac2ab8
Fernando Ferreira <fernando.rafael.ferreira@gmail.com>
2018-02-23 19:03:52 -0300

    Scrapes 'Blocos Partidários' from 'Câmara Legislativa.'

--- xeretas/camara.leg.br/blocos-partidos.js
+++ xeretas/camara.leg.br/blocos-partidos.js
@@ -1,7 +1,13 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const S = require('string');
+const date = require('date-and-time');
 
 const json = require('../../util/json');
+const crawlXml = require('../crawl-xml.js');
+
+const { Bloco, Partido, BlocoPartido } = require('../../model');
+
 
 
 const OUTPUT = 'data/camara.leg.br/blocos-partidarios.json';
@@ -11,18 +17,78 @@ const OUTPUT = 'data/camara.leg.br/blocos-partidarios.json';
  *
  * Blocos de Partidos com representação na Câmara dos Deputados.
  */
-
-function crawl() {
-    request(
-        { url: 'http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterPartidosBlocoCD',
-          qs: { idBloco: '', numLegislatura: '' } },
-        function (error, response, xml) {
-            if (error) return console.error(error);
-
-            var records = json.fromXml(xml, 'blocos bloco');
-            json.write(records, OUTPUT);
-        }
-    );
+function id(bloco, partido) {
+    var id = "";
+    if (bloco != null) {
+        id += bloco.constructor.name + "[" + bloco.id + "," + bloco.nome + "] ";
+    }
+    if (partido != null) {
+        id += partido.constructor.name + "[" + partido.id + "," + partido.sigla + "] ";
+    }
+    return id;
 }
 
-module.exports = crawl;
+module.exports = {
+    name: "Blocos Partidários",
+    describe: "Blocos de Partidos com representação na Câmara dos Deputados.",
+
+    command: crawlXml({
+        request: {
+            url: 'http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterPartidosBlocoCD',
+            qs: { idBloco: '', numLegislatura: '' }
+        },
+
+        select: 'bloco',
+
+        schema: (scrape) => ({
+            idCamara: scrape('idBloco').as.number(),
+            nome: scrape('nomeBloco').as.text(),
+            sigla: scrape('siglaBloco').as.text(),
+            criacao: scrape('dataCriacaoBloco').as.date(),
+            extincao: scrape('dataExtincaoBloco').as.date(),
+            partidos: scrape('partido').as((scrape) => ({
+                sigla: scrape('siglaPartido').as.text(),
+                nome: scrape('nomePartido').as.text(),
+                adesao: scrape('dataAdesaoPartido').as.date(),
+                desligamento: scrape('dataDesligamentoPartido').as.date(),
+            }))
+        }),
+
+        findOrCreate: function(bloco) {
+            return Bloco.findOrCreate({
+                where: { 'idCamara': bloco.idCamara },
+                defaults: bloco,
+                include: [ Partido ]
+            });
+        },
+
+        spread: function(bloco, parsed) {
+            var siglas = parsed.partidos.map((p) => p.sigla);
+            return Partido.findAll({
+                where: { sigla: siglas },
+                attributes: ['id', 'sigla', 'nome']
+            }).then((partidos) => {
+                var promises = [];
+                parsed.partidos.forEach((p) => {
+                    var partido = partidos.find(x => x.sigla == p.sigla);
+                    if (partido) {
+                        var promise = bloco.addPartido(partido, { through: p })
+                        .then(() => {
+                            console.debug("Associated", id(bloco, partido));
+                            return [bloco, partido];
+                        }).catch((e) => {
+                            console.error("Failed to associate", id(bloco, partido), "\n", e);
+                        });
+
+                        promises.push(promise);
+
+                    } else {
+                        console.error("Partido", p, "não existe no banco de dados.");
+                    }
+                });
+
+                return Promise.all(promises);
+            });
+        }
+    })
+}
