12dc7a3277db25a7d5c028763c449c9304725467
Fernando Ferreira <fernando.rafael.ferreira@gmail.com>
2018-03-05 16:37:57 -0300

    Crawls Legislatura from Dados Abertos' new web services.

---
+++ xeretas/camara.leg.br/legislatura.js
@@ -0,0 +1,36 @@
+const crawl = require('../crawl.js');
+
+const { Legislatura } = require('../../model');
+
+module.exports = {
+    name: "Legislatura",
+    describe: "São os períodos de trabalhos legislativos, iniciados no dia " +
+              "da posse dos parlamentares após uma eleição e encerrados na " +
+              "véspera da posse dos deputados seguintes.",
+
+    command: crawl.json({
+        request: {
+            url: 'https://dadosabertos.camara.leg.br/api/v2/legislaturas?itens=100',
+            headers: {
+                'Accept': 'application/json',
+                'Accept-Charset': 'utf-8'
+            }
+        },
+
+        select: 'dados',
+
+        schema: (scrape) => ({
+            idCamara: scrape('id').as.number(),
+            uri: scrape('uri').as.text(),
+            inicio: scrape('dataInicio').as.date('YYYY/MM/DD'),
+            fim: scrape('dataFim').as.date('YYYY/MM/DD')
+        }),
+
+        findOrCreate: function(legislatura) {
+            return Legislatura.findOrCreate({
+                where: { 'idCamara': legislatura.idCamara },
+                defaults: legislatura
+            });
+        }
+    })
+}
