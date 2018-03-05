e084f30227ad6ddb1210101a8b8d3ef6e7c7a988
Fernando Ferreira <fernando.rafael.ferreira@gmail.com>
2018-03-05 14:33:03 -0300

    Refactors crawl to make it flexible when parsing responses other than XML.

--- xeretas/crawl-xml.js
+++ xeretas/crawl-xml.js
@@ -1,108 +1,23 @@
-const request = require('request-promise-native');
-const cheerio = require('cheerio');
-const S = require('string');
-
 const scrape = require('../util/scrape');
+const crawl = require('./crawl.js');
 
-const { writeText } = require('../util/json');
-
-function createRequest(options) {
-    var request = options.request;
-    if (typeof request === 'string' || request instanceof String) {
-        request = {
-            url: request
-        }
-    }
-
-    if (!request['transform']) {
-        // Load the XML body with cheerio
-        request['transform'] = function(body, response, resolveWithFullResponse) {
-            // Save the raw response to the disk
-            var filename = S(request.url).chompLeft('https://')
-                .chompLeft('http://').replaceAll('/', '_').replaceAll(':', '');
-            filename = 'data/' + filename.s + '.xml';
-            writeText(body, filename);
-            console.debug("Reponse written to", filename);
-
-            // Scrape the body and return
-            var scraped = scrape.xml(options.select)
-                            .as(options.schema).scrape(body);
-            if (resolveWithFullResponse) {
-                response.scraped = scraped;
-                return response;
-            } else {
-                return scraped;
-            }
-        };
-
-    } else {
-        console.warn("Transformation overriden in request.transform. " +
-                     "Make sure to scrape response yourself.");
-    }
-
-    return request;
-}
-
-function findAndUpdateOrCreate(findOrCreate, record) {
-    var promise = findOrCreate(record)
-        .spread((object, created) => {
-            if (created) {
-                console.debug("Created", object.constructor.name, object.id);
-                return [object, record];
-            } else {
-                return object.update(record).then(
-                    (object) => {
-                        console.debug("Updated", object.constructor.name, object.id,
-                            "with the latest data.");
-                        return [object, record];
-                    }
-                );
-            }
-        });
-
-    return promise;
-}
 
 function crawlXml(options) {
-    const defaultOptions = {
-        findOrCreate: () => { throw Error("findOrCreate is not defined.") },
-        schema: () => { throw Error("schema is not defined.") },
-        spread: null
-    };
+    if (!options.scrape) {
+        // Scrape uses a schema to parse
+        if (!options.schema) {
+            throw Error("Option 'schema' is required.");
+        }
+        if (!options.select) {
+            throw Error("Option 'select' is required.");
+        }
 
-    options = Object.assign({}, defaultOptions, options);
-
-    return function() {
-        return request(createRequest(options))
-            .catch((error) => {
-                // TODO If it's a temporary problem, retry.
-                console.error(error);
-            }).then((records) => {
-                var promises = [];
-
-                if (!records) {
-                    return Promise.resolve();
-                }
-
-                records.forEach((record, i) => {
-                    // if (i > 0) { return };
-                    var promise = findAndUpdateOrCreate(options.findOrCreate, record);
-
-                    // Chain all options.spread in sequence.
-                    if (typeof options.spread == 'function') {
-                        promise = promise.spread(options.spread);
-                    }
-
-                    promise.catch((error) => {
-                        console.error(error);
-                    });
-
-                    promises.push(promise);
-                });
-
-                return Promise.all(promises);
-            });
+        options.scrape = (body) => scrape.xml(options.select)
+            .as(options.schema).scrape(body);
     }
+
+    return crawl(options);
 }
+
 
 module.exports = crawlXml;
---
+++ xeretas/crawl.js
@@ -0,0 +1,127 @@
+const request = require('request-promise-native');
+const S = require('string');
+
+const scrape = require('../util/scrape');
+
+const { writeText } = require('../util/json');
+const path = require('path');
+
+
+function dumpResponse(request, response) {
+    // Save the raw response to the disk
+    var filename = S(request.url).chompLeft('https://')
+        .chompLeft('http://').replaceAll('/', '_').replaceAll(':', '');
+
+    filename = path.join('data/responses/', filename.s);
+
+    writeText(response.body, filename);
+
+    console.debug("Reponse written to", filename);
+}
+
+function makeRequest(options) {
+    var req = options.request;
+    if (typeof req === 'string' || req instanceof String) {
+        req = {
+            url: req
+        }
+    }
+
+    transform = req['transform'];
+    req['transform'] = function(body, response, resolveWithFullResponse) {
+        // Save the raw response
+        dumpResponse(req, response);
+
+        // Scrape the body and return
+        if (transform || options.scrape) {
+            var scraped = transform ? transform(body, response, false)
+                            : options.scrape(body);
+            if (resolveWithFullResponse) {
+                response.scraped = scraped;
+                return response;
+            } else {
+                return scraped;
+            }
+        } else {
+            return resolveWithFullResponse ? response : body;
+        }
+    };
+
+    return request(req).catch((error) => {
+            // TODO If it's a temporary problem, retry.
+            console.error(error);
+        });
+}
+
+function findAndUpdateOrCreate(findOrCreate, record) {
+    var promise = findOrCreate(record);
+
+    if (promise == null) {
+        console.warn("Crawler findOrCreate returned null.");
+        return Promise.reject();
+    }
+
+    return promise.spread((object, created) => {
+            if (created) {
+                console.debug("Created", object.constructor.name, object.id);
+                return [object, record];
+            } else {
+                return object.update(record).then(
+                    (object) => {
+                        console.debug("Updated", object.constructor.name, object.id,
+                            "with the latest data.");
+                        return [object, record];
+                    }
+                );
+            }
+        });
+}
+
+function crawl(options) {
+    const defaultOptions = {
+        scrape: null,
+        schema: null,
+        select: '*',
+
+        findOrCreate: null,
+
+        spread: null
+    };
+
+    options = Object.assign({}, defaultOptions, options);
+
+    if (!options.findOrCreate) {
+        throw Error("Option 'findOrCreate' is required.");
+    }
+
+    return function() {
+        return makeRequest(options)
+            .then((records) => {
+                if (!records || !Array.isArray(records)) {
+                    console.warn("Incomprehensible Response:\n", records);
+                    return Promise.resolve(Error("Incomprehensible Response"));
+                }
+
+                var promises = [];
+                records.forEach((record, i) => {
+                    // if (i > 0) { return };
+                    var promise = findAndUpdateOrCreate(options.findOrCreate, record);
+
+                    // Chain all options.spread in sequence.
+                    if (typeof options.spread == 'function') {
+                        promise = promise.spread(options.spread);
+                    }
+
+                    promise.catch((error) => {
+                        console.error(error);
+                    });
+
+                    promises.push(promise);
+                });
+
+                return Promise.all(promises);
+            });
+    }
+}
+
+module.exports = crawl;
