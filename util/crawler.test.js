76e7bdb0391407dc76ee358767b357f42b76656e
Bharata <fernando.rafael.ferreira@gmail.com>
2018-06-12 14:36:29 -0400

    Adds Promise tests to Crawler.stepByStep().

--- util/crawler.test.js
+++ util/crawler.test.js
@@ -21,6 +21,7 @@ test("Crawler.stepByStep supports function steps.", () => {
     return expect(crawl()).resolves.toBe("Final Result")
 })
 
+// To fork means the next step will be executed once for each element in the returned Array
 test("Crawler.stepByStep forks when step returns an Array.", async () => {
     var crawl = crawler.stepByStep([
         (result) => [1, 2, 3, 4],
@@ -47,4 +48,42 @@ test("Crawler.stepByStep recursively forks with Array results.", async () => {
         ["the sun is alive", "the sun is flying"],
         ["the planet is alive", "the planet is flying"]
     ]))
+})
+
+// Returns a Promise that resolves to `resolution` after waiting for `wait` ms.
+function promise(resolution, wait) {
+    return new Promise(resolve => setTimeout(() => resolve(resolution), wait))
+}
+
+test("Crawler must wait for Promise before taking next step", async () => {
+    var crawl = crawler.stepByStep([
+        (result) => promise(["here", "present"], 50),
+        (result) => "be " + result
+    ])
+
+    var result = await crawl()
+    expect(result).toHaveLength(2)
+    expect(result).toEqual(expect.arrayContaining(["be here", "be present"]))
+})
+
+test("Crawler must wait for each Promise in Array before taking next step", async () => {
+    var crawl = crawler.stepByStep([
+        (result) => ["here", promise("present", 50), Promise.resolve("happy")],
+        (result) => "be " + result
+    ])
+
+    var result = await crawl()
+    expect(result).toHaveLength(3)
+    expect(result).toEqual(expect.arrayContaining(["be here", "be present", "be happy"]))
+})
+
+test("Crawler must wait for nested Promises to resolve before next step is taken", async () => {
+    var crawl = crawler.stepByStep([
+        (result) => ["here", promise([ "present", promise("happy", 50) ], 50)],
+        (result) => "be " + result
+    ])
+
+    var result = await crawl()
+    expect(result).toHaveLength(2)
+    expect(result).toEqual(expect.arrayContaining(["be here", ["be present", "be happy"]]))
 })
